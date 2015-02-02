f5c9262a8edd2215237d004b829efc8246b10827
nupurkapoor <nupurk15@gmail.com>
2015-02-01 22:05:49 -0500

    adding model todo nad collection todolist

---
+++ js/collections/todos.js
@@ -0,0 +1,50 @@
+
+  // a TodoList collection is used to group our models. 
+  // The collection uses the LocalStorage adapter to override Backbone’s default sync() operation 
+  // with one that will persist our Todo records to HTML5 Local Storage. 
+  // Through local storage, they’re saved between page requests.
+
+  var app = app || {};
+
+  // Todo Collection
+  // ---------------
+
+  // The collection of todos is backed by *localStorage* instead of a remote
+  // server.
+  var TodoList = Backbone.Collection.extend({
+
+    // Reference to this collection's model.
+    model: app.Todo,
+
+    // Save all of the todo items under the `"todos-backbone"` namespace.
+    localStorage: new Backbone.LocalStorage('todos-backbone'),
+
+    // Filter down the list of all todo items that are finished.
+    completed: function() {
+      return this.filter(function( todo ) {
+        return todo.get('completed');
+      });
+    },
+
+    // Filter down the list to only todo items that are still not finished.
+    remaining: function() {
+      return this.without.apply( this, this.completed() );
+    },
+
+    // We keep the Todos in sequential order, despite being saved by unordered
+    // GUID in the database. This generates the next order number for new items.
+    nextOrder: function() {
+      if ( !this.length ) {
+        return 1;
+      }
+      return this.last().get('order') + 1;
+    },
+
+    // Todos are sorted by their original insertion order.
+    comparator: function( todo ) {
+      return todo.get('order');
+    }
+  });
+
+  // Create our global collection of **Todos**.
+  app.Todos = new TodoList();
---
+++ js/models/todo.js
@@ -0,0 +1,25 @@
+// Todo Model
+// ----------
+// The basic todo model has `title` and `completed` attributes.
+// a title stores a todo item’s title and a completed status indicates if it’s complete. 
+// These attributes are passed as defaults
+
+var app = app || {}; //creating namespace
+
+  app.Todo = Backbone.Model.extend({
+
+    // Default attributes ensure that each todo created has `title` and `completed` keys.
+    defaults: {
+      title: '',
+      completed: false
+    },
+
+    // Toggle the `completed` state of this todo item.
+    // Through this method a Todo item’s completion status can be set and simultaneously persisted.
+    toggle: function() {
+      this.save({
+        completed: !this.get('completed')
+      });
+    }
+
+  });
